7b14b0d9c848f647a929f445433627b9bef4d732
Ashley Zhou <ashleyzhou@D-69-91-156-34.dhcp4.washington.edu>
2018-03-05 18:21:23 -0800

    scatter plot

--- ui.R
+++ ui.R
@@ -1,19 +1,25 @@
-library("shiny")
-library("plotly")
-library("dplyr")
-library("rsconnect")
-FPD.file <- na.omit(read.csv("./FPD.csv", check.names=FALSE))
-water.land.use <- select(FPD.file, "City Name", "Land Area (Square Miles) 2000", "Land Area (Square Miles) 2010", "Water Area (%) 2000", "Water Area (%) 2010")
-city.name <- as.character(water.land.use$"City Name")
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+library(rsconnect)
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+ui <- navbarPage("whattttt",
+  tabPanel("a",
+    # Application title
+    titlePanel("Occupancy Rate"),
+
+    # Sidebar with slider input that allows the users to choose the city that they are interested in
+    sidebarLayout(
+      sidebarPanel(
+        textInput(inputId = "city", label = "Choose the City")
+      ),
   
-  # Application title
-  titlePanel("Pie Chart for Land & Water Use in 2000 and 2010"),
-  
-      selectInput("city", "Choose the name of the city:", city.name,
-                  selected = "Benton"),
-      plotlyOutput("landwaterPlotly")
+      mainPanel(
+        plotlyOutput("plot")
       )
+    )
+  )
 )
+
 
